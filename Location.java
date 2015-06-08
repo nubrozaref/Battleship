c02442221d63e0447329706f650a79651838da80
nubrozaref <nubrozaref@gmail.com>
2015-06-08 08:32:00 -0700

    Create Location.java

---
+++ Location.java
@@ -0,0 +1,18 @@
+public class Location {
+
+	int x,y;
+
+	public Location(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX(){
+		return x;
+	}
+
+	public int getY(){
+		return y;
+	}
+	
+}
